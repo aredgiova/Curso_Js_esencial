54d5b61d0b0b8bd67d7fc77ed28f9d1432b7c68c
aredgiova <ared.lopez@gmail.com>
2022-09-14 01:47:46 -0500

    update1409

---
+++ parte3.js
@@ -0,0 +1,22 @@
+
+//****************************
+//***** Propiedades numericas */
+
+console.log("MAX_VALUE:", Number.MAX_VALUE);
+
+console.log("MIN_VALUE:", Number.MIN_VALUE);
+
+console.log("NEGATIVE_INFINITY:", Number.NEGATIVE_INFINITY);
+
+console.log("POSITIVE_INFINITY:", Number.POSITIVE_INFINITY);
+
+console.log("NaN:", Number.NaN); //saber si el datos es o no un numero
+
+var numero = "10.420";
+console.log("parseInt: ", parseInt(numero));
+console.log("parseFloat: ", parseFloat(numero));
+console.log("isNaN;", isNaN(numero));
+console.log("isInteger:", Number.isIntegrer(numero)); //valida si el numero es un entero
+
+console.log("toString:", numero.toString()); //convierte un numero en una cadena de texto
+
