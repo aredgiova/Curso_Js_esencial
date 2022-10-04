bafcc3baffa44559e7f61523b74ea5a20470e9f7
aredgiova <ared.lopez@gmail.com>
2022-10-04 13:27:32 -0500

    04-10-2022 (3)

--- parte4.js
+++ parte4.js
@@ -6,6 +6,9 @@
 /**METODOS DE GENERACION, REEMPLAZO Y SEPARACION */
 
 var mensaje = "Estoy aprendiento JavaScript";
+var mensaje2 = "y programacion"
+var total = 123456;
+
 var resultado;
  
 // repeat
@@ -23,5 +26,16 @@ var resultado;
 
 // trim
 // resultado = mensaje.trim() -> elimina los espacios en blanco antes y despues de la cadena de texto
+
+//resultado = total.toString(); -> convierte numeros en una cadena de texto
+
+//resultado = mensaje.toLowerCase(); -> convierte todo el texto en minuscula
+
+//resultado = mensaje.toUpperCase(); ->convierte todo el texto en mayuscula
+
+//resultado = mensaje.concat(mensaje2, "y tengo ideas"); ->une las cadenas de texto que se pongan en el metodo
+
+
+
 
 console.log(resultado);
---
+++ parte5.js
@@ -0,0 +1,18 @@
+//*****************
+//**** PLANTILLAS Y LITERALES
+
+var lenguaje = 'JavaScript';
+var lenguaje2 = 'HTML';
+
+var mensaje = `Me gusta ${lenguaje} y su integracion con ${lenguaje2}`; //ojo con las comillas NO SON COMILLAS NORMALES
+
+var mensajeMultimedia = `
+    Hola mundo, 
+    estoy aprendiendo
+    ${lenguaje} y me gusta
+    como se integra con HTML y CSS`;
+
+console.log(mensajeMultimedia);
+
+
+
