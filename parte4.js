a336978de10383d00baa24358e24cb6f7600617a
aredgiova <ared.lopez@gmail.com>
2022-10-04 12:51:12 -0500

    04-10-2022

---
+++ parte4.js
@@ -0,0 +1,23 @@
+/****** 
+/**METODOS DE GENERACION, REEMPLAZO Y SEPARACION */
+
+var mensaje = "Estoy aprendiento JavaScript";
+var resultado;
+ 
+// repeat
+// resultado = mensaje.repeat(5) ->repite un rexto determinado cantidad de veces
+
+// replace -> no afecta el valor original cambiado 
+// resultado = mensaje.replace("JavaScript", "a programar") ->busca la palabra JavaScript en el texto y lo cambia por "a programar"
+
+// slice
+// resultado = mensaje.slice(6) -> comienza el texto en el caracter selecionado 
+// resultado = mensaje.slice(6, mensaje,length-5) -> inicio y final a mostrar en un texto
+
+// split
+// resultado = mensaje.split(" ") -> separa la cadena de texto en un arreglo
+
+// trim
+// resultado = mensaje.trim() -> elimina los espacios en blanco antes y despues de la cadena de texto
+
+console.log(resultado);
