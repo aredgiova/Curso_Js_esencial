ecc85a5f5b2db1aa2919aa2c6f366902d08ec0a6
aredgiova <ared.lopez@gmail.com>
2022-12-13 00:54:57 -0500

    13/12/2022

---
+++ parte7.js
@@ -0,0 +1,9 @@
+"use strict"
+
+//********* Iterando con arreglos (con for in)****/
+
+var platillos = ["ceviche", "tacos", "pasta"];
+
+for (let platillo in platillos){
+    console.log(platillos[platillo])
+}
